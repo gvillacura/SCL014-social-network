5732a4dadbc76b903bed5a309252d2529abe2020
lady pino <ladypino112@gmail.com>
2020-08-15 20:12:12 -0400

    Co-authored-by: freyesmar@hotmail.com <freyesmar@hotmail.com>

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,4 +1,36 @@
 const db = firebase.firestore();
+var storage = firebase.storage();
+var storageRef = storage.ref();
+// var imagesRef = storageRef.child('images/');
+
+
+export const uploadFile = (archivoImg) => {
+    console.log('se a recibido el archivo')
+    var file = archivoImg;
+    var metadata = {
+     contentType: 'images/jpeg'
+
+    };
+    var tareaSubir = storageRef.child('images/' + file.name).put(file,metadata);
+    tareaSubir.on(firebase.storage.TaskEvent.STATE_CHANGED,
+        function(snapshot) {
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+              case firebase.storage.TaskState.PAUSED: // or 'paused'
+                console.log('Upload is paused');
+                break;
+              case firebase.storage.TaskState.RUNNING: // or 'running'
+                console.log('Upload is running');
+                break;
+            }
+          })
+
+   
+}
+
+ 
 
 // Función para obtener fecha y hora.
 const currentTime = () => {
@@ -171,3 +203,4 @@ export const containerPost = () => {
         });
     });
 };
+
